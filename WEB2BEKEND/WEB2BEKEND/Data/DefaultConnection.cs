f4c10d655d19b3b2dfaece6488b91f681f0aaaab
Zika98 <61841559+Zika98@users.noreply.github.com>
2021-06-23 15:22:30 +0200

    baza

---
+++ WEB2BEKEND/WEB2BEKEND/Data/DefaultConnection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB2BEKEND.Models;
+
+namespace WEB2BEKEND.Data
+{
+    public class DefaultConnection : DbContext
+    {
+        
+
+        public DefaultConnection (DbContextOptions<DefaultConnection> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
---
+++ WEB2BEKEND/WEB2BEKEND/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB2BEKEND.Models
+{
+    public class User
+    {
+        [Key]       
+        public string Username { get; set; }
+    
+        public string Password { get; set; }
+      
+        public string NameAndLastname { get; set; }
+      
+        public DateTime BirthDate { get; set; }
+        
+        public string Address { get; set; }
+        
+        public string ImageData { get; set; }
+        
+        public string Email { get; set; }
+       
+        public string UserType { get; set; }
+
+        public string ActiveStatus { get; set; }
+
+
+        public User()
+        {
+            Username = "+";
+            Password = "+";
+            NameAndLastname = "+";
+            BirthDate = DateTime.Now;
+            Address = "+";
+            Email = "+";
+            UserType = "+";
+            ActiveStatus = "Pending";
+        }
+    }
+}
